a4efabd9c7ea2ced9f96cf2bf1a76d808967c92a
Richard <richardhayes777@gmail.com>
2016-11-01 16:55:38 +0000

    script for resizing to fit android densities

---
+++ resize_android.sh
@@ -0,0 +1,6 @@
+convert $1 -resize 100% drawable-xxxhdpi/$1
+convert $1 -resize 75% drawable-xxhdpi/$1
+convert $1 -resize 50% drawable-xhdpi/$1
+convert $1 -resize 33% drawable-hdpi/$1
+convert $1 -resize 17% drawable-mdpi/$1
+
